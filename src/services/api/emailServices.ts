02d9185eace9810d2864a516698dd5d7c0a4303f
Chhakuli <chhakulizingare12@gmail.com>
2024-10-13 07:45:16 +0530

    feat: integrate pagination api of emails

--- src/services/api/emailServices.ts
+++ src/services/api/emailServices.ts
@@ -1,6 +1,9 @@
+import { EmailsResponse } from '@/interface/email';
+
 import { get } from '../httpService';
 
-const fetchEmails = () => get('flipkart-email-mock.now.sh/');
+const fetchEmails = (page: number = 1): Promise<EmailsResponse> =>
+  get(`flipkart-email-mock.now.sh/?page=${page}`);
 
 const fetchEmailDetails = (id: number) =>
   get(`flipkart-email-mock.now.sh/?id=${id}`);
