f33e5c7f6b6383037be1a2bc1dfba63e835ebba4
Chhakuli <chhakulizingare12@gmail.com>
2024-10-13 07:44:47 +0530

    feat: add topbar for all mail, read, unread, favorite filter buttons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,11 @@ import type { Metadata, Viewport } from 'next';
 
 import '@/styles/globals.css';
 
-import { FavoritesProvider } from '@/context/favoritesContext';
+import { EmailProvider } from '@/context/EmailContext';
 
 import { siteConfig } from '@/config/site';
 import { inter } from '@/lib/fonts';
+import { TopBar } from '@/components/emails/TopBar';
 
 export const metadata: Metadata = {
   title: {
@@ -27,8 +28,13 @@ type RootLayoutProps = Readonly<{
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={`min-h-screen antialiased ${inter.className}`}>
-        <FavoritesProvider>{children}</FavoritesProvider>
+      <body
+        className={`min-h-screen bg-background antialiased ${inter.className}`}
+      >
+        <EmailProvider>
+          <TopBar />
+          {children}
+        </EmailProvider>
       </body>
     </html>
   );
