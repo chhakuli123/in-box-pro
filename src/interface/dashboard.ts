d8baddfa22e2e1a31a47ba4c09f9f6638262028d
Chhakuli <chhakulizingare12@gmail.com>
2024-10-19 09:34:41 +0530

    feat: add function to call mockdata

---
+++ src/interface/dashboard.ts
@@ -0,0 +1,11 @@
+export interface DataEntry {
+  date: string; // Format: "DD/MM/YYYY"
+  age: string; // e.g., "15-25" or ">25"
+  gender: string; // "Male" or "Female"
+  A: number;
+  B: number;
+  C: number;
+  D: number;
+  E: number;
+  F: number;
+}
